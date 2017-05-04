411909c4652d76001e67165b9098924a5f2f1ce1
Michael Nguyen <nguyen.michl@gmail.com>
2017-05-04 18:38:19 -0500

    finalize structure of tables in database.

--- database/schema-proposed.sql
+++ database/schema-proposed.sql
@@ -4,27 +4,36 @@ CREATE DATABASE WishListApp;
 USE WishListApp;
 
 CREATE TABLE User (
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(id),
+    userId INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (userId)
+    -- User attributes
 );
 
 CREATE TABLE Item (
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(id),
+    itemId INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (itemId)
+    -- Item Attributes
 );
 
-CREATE TABLE Contack (
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(id),
+CREATE TABLE Contact (
+    contactId INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (contactId)
+    -- Contact Attributes
 );
 
-CREATE TABLE User (
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(id),
+CREATE TABLE WishList (
+    wishListId INT NOT NULL AUTO_INCREMENT,
+    userId INT NOT NULL,
+    PRIMARY KEY (wishListId),
+    FOREIGN KEY (userId) REFERENCES User(userId)
+    -- WishList Attributes
 );
 
-CREATE TABLE User (
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(id),
+CREATE TABLE ContactList (
+    contactListId INT NOT NULL AUTO_INCREMENT,
+    userId INT NOT NULL,
+    PRIMARY KEY(contactListId),
+    FOREIGN KEY (userId) REFERENCES User(userId)
+    -- Contact List Attributes
 );
 
