380438b13b208c3e71b647eea72d16720c82818b
Jeff McMahan <mcmahan.jeff@gmail.com>
2017-05-04 18:17:44 -0500

    html and post/db route files added to routes dir **files not complete**

---
+++ routes/html-routes.js
@@ -0,0 +1,17 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+var path = require("path");
+
+// Routes
+module.exports = function(app) {
+
+    // index route loades index.html
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+
+};
---
+++ routes/post-api-routes.js
@@ -0,0 +1,13 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+var db = require("../models");
+
+// routes
+module.exports = function(app) {
+    // GET, POST, PUT & DELETE routes go here
+
+
+};
