681f6007c2ce9f86181f57f721429aa7216fec9d
Zach Zador <zazador@gmail.com>
2017-05-04 19:58:47 -0500

    Initial authorization attempt. User is created, but callback fails

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,17 +4,35 @@
 
 // Dependencies
 var path = require("path");
+var passport = require('passport');
+var env = {
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CALLBACK_URL: 'http://localhost:8080/callback'
+};
 
 // Routes
 module.exports = function(app) {
 
+	// We are also going to implement the callback route which will redirect the logged in user to the polls page if authentication succeeds.
+	app.get('/callback',
+      passport.authenticate('auth0', { failureRedirect: '/' }),
+      function(req, res) {
+        res.redirect(req.session.returnTo || '/cms');
+    });
+
+    app.get('/login',function(req, res){
+  // Same thing for the login page.
+  res.render('login', { env: env });
+});
+
     // index route loades index.html
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+    	res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     app.get("/cms", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/cms.html"));
+    	res.sendFile(path.join(__dirname, "../public/cms.html"));
     });
 
 };
