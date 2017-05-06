4210ee4e2d1997cd7ba057fbe4cf79ccb9deea32
Jeff McMahan <mcmahan.jeff@gmail.com>
2017-05-06 13:08:11 -0500

    walmart ebay api

--- helpers/ebay.js
+++ helpers/ebay.js
@@ -1,17 +1,21 @@
 var request = require("request");
 
-var search = 'https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=JeffMcMa-WishList-PRD-2090fc79c-bc5ccb42&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=iPhone&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-US&siteid=0'
-
-request(search,
-
-    function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(JSON.parse(body).findItemsByKeywordsResponse[0].searchResult[0].item[0].title[0]);
-            console.log(JSON.parse(body).findItemsByKeywordsResponse[0].searchResult[0].item[0].viewItemURL[0]);
-            console.log(JSON.parse(body).findItemsByKeywordsResponse[0].searchResult[0].item[0].sellingStatus[0].currentPrice[0].__value__);
 
 
-        }
-        return body;
-    });
-console.log("working?")
+module.exports = function(app) {
+    var userInput = "iphone5";
+
+    var search = 'https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=JeffMcMa-WishList-PRD-2090fc79c-bc5ccb42&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=' + userInput + '&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-US&siteid=0';
+    request(search,
+
+        function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log("\n" + "=================================ebay==========================================");
+                console.log("Item: " + JSON.parse(body).findItemsByKeywordsResponse[0].searchResult[0].item[0].title[0]);
+                console.log("Product Page URL: " + JSON.parse(body).findItemsByKeywordsResponse[0].searchResult[0].item[0].viewItemURL[0]);
+                console.log("Product Image URL: " + JSON.parse(body).findItemsByKeywordsResponse[0].searchResult[0].item[0].galleryURL[0]);
+                console.log("Price: $" + JSON.parse(body).findItemsByKeywordsResponse[0].searchResult[0].item[0].sellingStatus[0].currentPrice[0].__value__ + "\n");
+                console.log("================================================================================");
+            }
+        });
+};
--- helpers/walmart.js
+++ helpers/walmart.js
@@ -1,15 +1,35 @@
 var request = require("request");
 
-var search = 'http://api.walmartlabs.com/v1/search?query=ipod&format=json&apiKey=gv4fj48wed5epggnuwnrjt8c&numItems=1';
 
-request(search,
+module.exports = function(app) {
+    var wishListItem = "iphone6s";
 
-    function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log("Product Name: " + JSON.parse(body).items[0].name);
-            console.log("Sale Price: $" + JSON.parse(body).items[0].salePrice + ".00");
-            console.log("Product URL: " + JSON.parse(body).items[0].productUrl);
-        }
-        return body;
-    });
-console.log("working?")
+    var search = 'http://api.walmartlabs.com/v1/search?query=' + wishListItem + '&format=json&apiKey=gv4fj48wed5epggnuwnrjt8c&numItems=3';
+
+    request(search,
+
+        function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var walmartArray = [];
+                for (var i = 0; i < JSON.parse(body).items.length; i++) {
+                    walmartArray.push({ "name": JSON.parse(body).items[i].name }, { "salePrice": parseInt(JSON.parse(body).items[i].salePrice) }, { "productUrl": JSON.parse(body).items[i].productUrl });
+
+
+                    // console.log("=======================Walmart" + i + "===================================");
+                    // console.log("\n" + "Product Name: " + JSON.parse(body).items[i].name);
+                    // console.log("Price: $" + JSON.parse(body).items[i].salePrice);
+                    // // console.log("Product URL: " + JSON.parse(body).items[i].productUrl + "\n");
+                    // console.log("=================================================================");
+                }
+                console.log(walmartArray);
+                var lowestPrice = Math.min.apply(Math, walmartArray.map(function(o) { return o.salePrice; }));
+                console.log("Low: " + lowestPrice);
+                var walmartObj = walmartArray.find(function(o) { return o.salePrice == lowestPrice; });
+                console.log("Lowest price Item: " + walmartObj);
+
+            }
+
+        });
+};
+
+// use: require("./helpers/walmart.js")(app); in file that will use walmart api object
