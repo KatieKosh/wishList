f179f3a510e2fab5be557a2cd95cde31205f3e80
Michael Nguyen <nguyen.michl@gmail.com>
2017-05-09 05:00:40 -0500

    modifications to test route. need to test

--- database/routing-mockup/crud-routes.js
+++ database/routing-mockup/crud-routes.js
@@ -1,43 +1,8 @@
 // Dependent on Sequelize Model.
 var db = require("../models");
 
-// Routes as export
-module.exports = function (app) {
-    // GET, POST, PUT & DELETE routes go here
-/* 
-JSON: req.body...
-{
-    authID: string,
-    title: string,
-    emails: string,
-    category: string
-}
-*/
-
-    // Initial Creation Route
-    // Change pointer as neccessary.
-    app.post("", function (req, res) {
-
-    });
-};
-
-/*
-Routes needed:
-- Create User, Wishlist, Contact List on Submit from Form
-    Inserting User into user table, assign name and authId.
-    Include and Link a wishlist
-        Wishlist Title and category
-    Include and Link a contact list
-        Create contacts and list as well.
-    
-- Helper Function to pull emails.
-*/
-
-
-
 // Helper functions: Turn Email comma lists into array of json-like objects to pass into Sequelize
-
-// expects string of emails, separated by commas
+// Expects string of emails, separated by commas
 // Returns trimmed array of emails.
 function formatEmailList(emailList) {
     var rawList = emailList;
@@ -48,3 +13,65 @@ function formatEmailList(emailList) {
 
     return returnArray;
 }
+
+
+// Routes as export
+module.exports = function (app) {
+    // GET, POST, PUT & DELETE routes go here
+    /* 
+    Assumptions for req.body:
+    JSON: req.body...
+    {
+        authId: string,
+        title: string,
+        emails: string,
+        category: string,
+        ...etc
+    }
+    */
+
+    // Initial Creation Route
+    // Change pointer as neccessary.
+    app.post("", function (req, res) {
+        // Repackage request body for readability
+        var attribute = {
+            userName: req.body.name,
+            userAuthId: req.body.authId,
+            wishlistTitle: req.body.title,
+            wishlistCategory: req.body.category,
+            rawEmails: req.body.emails
+        };
+
+        var emailArray = formatEmailList(attribute.rawEmails);
+
+        // Create user row, assign unique authO ID
+        db.User.create(
+            {
+                authId: attribute.userAuthId,
+                name: attribute.userName
+            }
+        // After user row created...
+        ).then(function(user) {
+        // Create and associate contact list.
+        // Create and associate contacts to contacts list.
+            user.createContactlist({}).then(function(contactlist) {
+                emailArray.forEach( function(email){
+                    contactlist.createContact(
+                        {
+                            email: email
+                        });
+                });
+            });
+        // Create and associate wishlist.
+            user.createWishlist(
+                {
+                    title: attribute.wishlistTitle,
+                    category: attribute.wishlistCategory
+                }
+            );   
+        }).then( function(){
+            // What to do after.
+        });
+    });
+};
+
